fc3da875742611bf16bf26c7530e26485a3170b6
Kai D. Gonzalez <gkai70263@gmail.com>
2024-07-29 15:36:09 -0400

    add settings to spawnmenu

---
+++ lua/autorun/client/settings.lua
@@ -0,0 +1,27 @@
+hook.Add( "AddToolMenuCategories", "CustomCategory", function()
+	spawnmenu.AddToolCategory( "Utilities", "Armored NPCs", "#Armored NPCs" )
+end )
+
+hook.Add( "PopulateToolMenu", "CustomMenuSettings", function()
+	local super = LocalPlayer():IsSuperAdmin()
+
+	spawnmenu.AddToolMenuOption( "Utilities", "Armored NPCs", "ArmoredNPCSettings", "#Settings", "", "", function( panel )
+		if not super then
+			panel:ClearControls()
+			panel:Help( "Only super admins can change Armored NPC settings." )
+			return
+		end
+		panel:ClearControls()
+		panel:CheckBox( "Enabled", "gk_armor_enabled" )
+		panel:CheckBox( "Send Messages To Server Console", "gk_send_messages" )
+
+		panel:Help( "The type of protection. nodmg is more like blast protection, deplete just lessens the damage." )
+		local cbox = panel:ComboBox("Protection Type", "gk_protection_type")
+		cbox:AddChoice("nodmg")
+		cbox:AddChoice("deplete")
+
+		cbox:ChooseOptionID(1)
+
+		panel:NumSlider( "Super Soldier Modifier", "gk_super_soldier_modifier", 0, 1, 2, "%" )
+	end )
+end )
